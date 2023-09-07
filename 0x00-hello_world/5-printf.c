e87eb47bd6ad3de65d6a9df83e204d7e8c60f1a8
Ahmed <ahmed.com>
2023-09-07 15:23:36 -0700

    script

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /** 
- *Main - Entry point
+ * main - Entry point
  *
- *Description: A C program that prints with printf function
+ * Description: A C program that prints with printf function.
  *
- *Return: Always 0(Seccess)
+ * Return: Always 0 (Success)
 */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
