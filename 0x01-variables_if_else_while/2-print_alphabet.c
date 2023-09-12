d0a37592f412a5356a544209b1d9a337dbca6212
Ahmed <ahmed.com>
2023-09-11 20:45:27 -0700

    script

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,21 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - entry point
- *
- * Description: print all alphabet letters
+ * main - Prints the alphabet in lowercase.
  *
  * Return: Always 0.
-*/
-
+ */
 int main(void)
 {
-	char ch = 'a';
-	while (ch <= 'z')
-	{
-		putchar(ch);
-		ch++;
-	}
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
 	putchar('\n');
+
 	return (0);
 }
