fd41bc67fd8dac56ba98fdba5effc6ed2152b68c
Ahmed <ahmed.com>
2023-09-19 07:54:27 -0700

    script

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+#include <stdio.h>
+
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+*/
+
+int main(void)
+{
+
+	int c;
+
+	if (c >= "A" && c <= "Z")
+		return (1);
+
+	return (0);
+}
