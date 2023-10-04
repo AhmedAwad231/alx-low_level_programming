25c9b657b0fa1b7c5e96514f5b090f75dcf592ee
Ahmed <ahmed.com>
2023-10-04 12:58:39 -0700

    script

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,16 @@
-#!/bin/bash
-int main(char **argv)
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * @argc: Counts the number of parameters that go into main
+ * @argv: Pointer of array of pointers containing strings entering main
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char **argv)
 {
-	printf("%s\n",argv[0]);
-	return 0;
+	if (argc > 0)
+		printf("%s\n", argv[0]);
+	return (0);
 }
