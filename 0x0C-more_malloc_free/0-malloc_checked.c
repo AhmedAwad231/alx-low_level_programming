7c20cd7f40112b0872296c11a36012a2ecbe4dda
Ahmed <ahmed.com>
2023-10-06 22:41:27 -0700

    script

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -10,10 +10,10 @@
  */
 void *malloc_checked(unsigned int b)
 {
-	int *ptr;
+	int *m = malloc(b);
 
-	ptr = malloc(b);
-	if (ptr == NULL)
+	if (m == 0)
 		exit(98);
-	return (ptr);
+
+	return (m);
 }
